0e6153317d487f942d58aa69316f4ec1d4e2252f
Vyshnav K B <vyshnavkb648@gmail.com>
2025-12-30 12:25:57 +0530

    updated code for  number guessing game

--- Guess the number/number_guessing_game.py
+++ Guess the number/number_guessing_game.py
@@ -1,34 +1,29 @@
-#NUMBER GUESSING GAME
-
 import random
 
-print("Hi! Welcome to the Number Guessing Game.\nYou have 7 chances to guess the number. Let's start!")
+print("Hi! Welcome to the Number Guessing Game.")
+print("You have 7 chances to guess the number.\n")
 
-lower_bound = int(input("Enter a lower bound of the range : "))
-upper_bound = int(input("Enter a upper bound for tht range : "))
+lower_bound = int(input("Enter a lower bound of the range: "))
+upper_bound = int(input("Enter an upper bound of the range: "))
 
-print(f"\nYou have 7 chances to guess the number between {lower_bound} and {upper_bound}. Let's start!")
+num = random.randint(lower_bound, upper_bound)
+chances = 7
+attempts = 0
 
-num=random.randint(lower_bound,upper_bound)
-ch=7
-gc=0
+while attempts < chances:
+    attempts += 1
+    guess = int(input("Enter your guess: "))
 
-while gc < ch:
-    gc+=1
-    guess=int(input("Enter your guess : "))
-
-    if guess==num:
-        print(f"Correct! The number is {num}. You guessed it in {gc} attempts.")
-
-    elif gc >= ch and gc != num:
-        print(f"Sorry! The number was {num}. Better luck next time.")
+    if guess == num:
+        print(f"🎉 Correct! The number is {num}. You guessed it in {attempts} attempts.")
+        break
 
     elif guess > num:
-        print("Too high! Try a lower number. ")
-
-    elif  guess < num:
-        print("Too low! Try a higher number.")
+        print("Too high! Try a lower number.")
 
     else:
-        print("You ran out of chances..")
-        break
+        print("Too low! Try a higher number.")
+
+# runs only if loop ends WITHOUT break
+if attempts == chances and guess != num:
+    print(f"❌ Sorry! You ran out of chances. The number was {num}.")
